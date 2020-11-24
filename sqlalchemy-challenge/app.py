552fa3addaf78b36a22f1c07163618963903f6b5
Grant Hicks <70181086+ghicks6@users.noreply.github.com>
2020-11-23 21:45:42 -0500

    Add files via upload

---
+++ sqlalchemy-challenge/app.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create Session (Link) From Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
